02e1d6187d513ef6b95b995f1edf2b2c98c16c4d
Strevino2 <strevino2@hotmail.com>
2022-03-26 14:42:53 -0500

    main

--- app.js
+++ app.js
@@ -87,3 +87,88 @@
                 }
             }   
     }
+
+    const completeTodos = () => {
+        removeElements ()
+        let newArr1 = arrayOfTodos.filter((item1)=> {
+            console.log(item1.completed)
+            return item1.completed == true
+        })
+        console.log(newArr1)
+        let ol = document.getElementById('todo-list')
+        for (let index = 0; index < newArr1.length; index++) {
+            if (newArr1[index].completed == true) {
+                
+                const obj = newArr1[index];
+                console.log(obj)
+                console.log(obj.title)
+                let li = document.createElement('li')
+                var title = document.createTextNode(obj.title)
+                li.appendChild(title);
+                ol.appendChild(li);
+            }
+        }  
+    }
+
+    const notCompleteTodos = () => {
+        removeElements ()
+        let newArr2 = arrayOfTodos.filter((item2)=> {
+            console.log(item2.completed)
+            return item2.completed == false
+        })
+        console.log(newArr2)
+        let ol = document.getElementById('todo-list')
+        for (let index = 0; index < newArr2.length; index++) {
+            if (newArr2[index].completed == false) {
+                
+                const obj = newArr2[index];
+                console.log(obj)
+                console.log(obj.title)
+                let li = document.createElement('li')
+                var title = document.createTextNode(obj.title)
+                li.appendChild(title);
+                ol.appendChild(li);
+            }
+        }  
+    }
+
+    
+    const separateTodos = () => {
+        removeElements ()
+        let newArr2 = arrayOfTodos.filter((item2)=> {
+            console.log(item2.completed)
+            return item2.completed == false
+        })
+        console.log(newArr2)
+        let ol = document.getElementById('todo-list')
+        for (let index = 0; index < newArr2.length; index++) {
+            if (newArr2[index].completed == false) {
+                
+                const obj = newArr2[index];
+                console.log(obj)
+                console.log(obj.title)
+                let li = document.createElement('li')
+                var title = document.createTextNode(obj.title)
+                li.appendChild(title);
+                ol.appendChild(li);
+            }
+        }  
+        let newArr1 = arrayOfTodos.filter((item1)=> {
+            console.log(item1.completed)
+            return item1.completed == true
+        })
+        console.log(newArr1)
+        let ol1 = document.getElementById('completeTodos')
+        for (let index = 0; index < newArr1.length; index++) {
+            if (newArr1[index].completed == true) {
+                
+                const obj = newArr1[index];
+                console.log(obj)
+                console.log(obj.title)
+                let li = document.createElement('li')
+                var title = document.createTextNode(obj.title)
+                li.appendChild(title);
+                ol1.appendChild(li);
+            }
+        }  
+    }
